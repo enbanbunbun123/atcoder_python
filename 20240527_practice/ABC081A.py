4d1a3ba1e640b600301e8ae12c934d3e58342fb6
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2024-05-27 16:25:18 +0900

    ABC081A

---
+++ 20240527_practice/ABC081A.py
@@ -0,0 +1,13 @@
+def placingMarbles(numbers):
+    count = 0
+
+    for i in range(len(numbers)):
+        if numbers[i] == 1:
+            count += 1
+
+    return count
+
+numbers = list(map(int, input().strip()))
+
+result = placingMarbles(numbers)
+print(result)
