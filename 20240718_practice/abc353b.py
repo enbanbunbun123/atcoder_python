af176d12b591ef22a6f44f6dc593ef27afaf6a31
enbanbunbun123 <enbanbunbun-123@docomo.ne.jp>
2024-07-18 22:54:14 +0900

    20240718_practice_353b.py

---
+++ 20240718_practice/abc353b.py
@@ -0,0 +1,16 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+
+left = K
+count = 0
+
+for a in A:
+
+    #残り席数が人数よりも少ない時に、次の回に乗車し発射回数を１つ増やす
+    if left < a:
+        count += 1
+        left = K
+    left -= a
+count += 1
+
+print(count)
